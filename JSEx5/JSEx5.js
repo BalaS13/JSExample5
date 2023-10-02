62f8811ac477fe8f4aac7097b68717767368be7a
Bala <c.durai.delhi@gmail.com>
2023-10-02 21:11:44 +0530

    Defining variables using var let const keywords

---
+++ JSEx5/JSEx5.js
@@ -0,0 +1,7 @@
+var valueOne   = 10;
+let valueTwo   = 20;
+const valueThree = 30;
+console.log("Var Value is : ",valueOne);
+console.log("Let Value is : ",valueTwo);
+console.log("Const Value is : ",valueThree);
+
