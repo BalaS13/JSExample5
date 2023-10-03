3cefe39f874c83ac9acb09809a569fde608054f0
Bala <c.durai.delhi@gmail.com>
2023-10-03 14:23:25 +0530

    Defining a variable using var keyword!

--- JSEx5/JSEx5.js
+++ JSEx5/JSEx5.js
@@ -1,7 +1,2 @@
-var valueOne   = 10;
-let valueTwo   = 20;
-const valueThree = 30;
-console.log("Var Value is : ",valueOne);
-console.log("Let Value is : ",valueTwo);
-console.log("Const Value is : ",valueThree);
-
+var valueOne = 10;
+console.log(valueOne);
